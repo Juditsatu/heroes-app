b597e78372f58d6e4e7218e3ae801a71d97034a6
Judit Santos <judit.sant@gmail.com>
2022-08-12 12:27:43 +0200

    Added edit & delete hero

--- src/app/heroes/pages/agregar/agregar.component.ts
+++ src/app/heroes/pages/agregar/agregar.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { switchMap } from 'rxjs';
+
 import { Heroe, Publisher } from '../../interface/heroes.interface';
 import { HeroesService } from '../../services/heroes.service';
 
@@ -6,45 +9,68 @@ import { HeroesService } from '../../services/heroes.service';
   selector: 'app-agregar',
   templateUrl: './agregar.component.html',
   styles: [
-  ]
+    `
+    img {
+      width: 100%;
+      border-radius: 5px;
+    }
+    `
+  ],
 })
 export class AgregarComponent implements OnInit {
+  publishers = [
+    {
+      id: 'DC Comics',
+      desc: 'DC - Comic',
+    },
+    {
+      id: 'Marvel Comics',
+      desc: 'Marvel - Comic',
+    },
+  ];
 
-publishers = [
-  {
-    id: 'DC Comics',
-    desc: 'DC - Comic'
-  },
-  {
-    id: 'Marvel Comics',
-    desc: 'Marvel - Comic'
-  }
-];
+  heroe: Heroe = {
+    superhero: '',
+    alter_ego: '',
+    characters: '',
+    first_appearance: '',
+    publisher: Publisher.DCComics,
+    alt_img: '',
+  };
 
-heroe: Heroe = {
-  superhero: '',
-  alter_ego: '',
-  characters: '',
-  first_appearance: '',
-  publisher: Publisher.DCComics,
-  alt_img: ''
-}
-
-constructor( private heroesService: HeroesService ) { }
-
-guardar() {
-
-  if( this.heroe.superhero.trim().length === 0) {
-    return
-  }
-
-  this.heroesService.agregarHeroe( this.heroe )
-    .subscribe(resp => console.log('response', resp))
-
-}
-
+  constructor(
+    private heroesService: HeroesService,
+    private activatedRoute: ActivatedRoute,
+    private router: Router
+  ) {}
 
   ngOnInit(): void {
+
+    if (!this.router.url.includes('editar')) {
+      return;
+    }
+
+    this.activatedRoute.params
+      .pipe(switchMap(({ id }) => this.heroesService.getHeroeById(id)))
+      .subscribe((heroe) => (this.heroe = heroe));
   }
 
+  guardar() {
+    if (this.heroe.superhero.trim().length === 0) {
+      return;
+    }
+
+    if (this.heroe.id) {
+      this.heroesService.actualizarHeroe(this.heroe)
+        .subscribe(heroe => console.log('Actualizando', heroe))
+    } else {
+      this.heroesService.agregarHeroe(this.heroe)
+        .subscribe(heroe => this.router.navigate(['/heroes/editar', heroe.id]));
+    }
+  }
+
+  borrar() {
+    this.heroesService.borrarHeroe(this.heroe.id!)
+      .subscribe(resp => this.router.navigate(['/heroes/list']))
+  }
 }
